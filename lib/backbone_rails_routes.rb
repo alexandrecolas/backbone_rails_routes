96df91ddfebf765d35261256b0591dc47dfaa6c7
Alexandre Colas <colasalexandre@gmail.com>
2013-09-26 22:15:14 +0000

    Add Rails::Engine.

--- lib/backbone_rails_routes.rb
+++ lib/backbone_rails_routes.rb
@@ -3,6 +3,11 @@ require "backbone_rails_routes/routes"
 
 module BackboneRailsRoutes
 
+  module Rails
+    class Engine < ::Rails::Engine
+    end
+  end
+
   def self.all
     return BackboneRailsRoutes::Routes.backbone.to_json
   end
